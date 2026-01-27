598fbb29fae5ad42b84199890a9b59714b79d504
Bruno Perciani <117026795+BrunoPerciani@users.noreply.github.com>
2026-01-27 14:39:43 -0300

    Create basic-select_titles-from-books.sql

---
+++ basic-select_titles-from-books.sql
@@ -0,0 +1,16 @@
+-- ============================================
+-- 1. Task Description
+-- Practice using the SELECT and FROM clauses
+-- to retrieve data from a table.
+-- The goal of this exercise is to return
+-- all book titles stored in the books table.
+-- 
+-- 2. Topics Covered
+-- - SELECT (field selection)
+-- - FROM (table selection)
+-- - Basic query structure
+-- ============================================
+
+-- Return all titles from the books table
+SELECT title
+FROM books;
