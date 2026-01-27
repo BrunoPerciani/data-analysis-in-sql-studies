8910040284a044b82d941bfc63c9efebd7a093f3
Bruno Perciani <117026795+BrunoPerciani@users.noreply.github.com>
2026-01-27 14:53:55 -0300

    Create 1.1.3 Basic Select: all-fields-from-books.sql

---
+++ 1.1.3 Basic Select: all-fields-from-books.sql
@@ -0,0 +1,17 @@
+-- ============================================
+-- 1. Task Description
+-- Practice using the SELECT and FROM clauses
+-- to retrieve information from the books table.
+-- The goal of this exercise is to return all
+-- available fields from every book in the table.
+--
+-- 2. Topics Covered
+-- - SELECT (field selection)
+-- - FROM (table selection)
+-- - Selecting all fields using the asterisk (*)
+-- - Basic query structure
+-- ============================================
+
+-- Select all fields from the books table
+SELECT *
+FROM books;
